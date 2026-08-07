18736a2405ef339ede0725754de7f4e5d5f5cde3
korvilp-sudo <korvil.p@gmail.com>
2026-08-07 18:51:16 -0300

    Create Calibration.js

---
+++ kai-core/lab/k-riador/cgm/Calibration.js
@@ -0,0 +1,6 @@
+kai-core/lab/k-riador/cgm/
+├──  <- Tela de calibragem
+├── GestureDetector.js <- Detecta pinch + mão verde
+├── cgm.config.js <- Salva os valores calibrados
+└── utils/
+    └── permissions.js <- Pede câmera + acessibilidade
